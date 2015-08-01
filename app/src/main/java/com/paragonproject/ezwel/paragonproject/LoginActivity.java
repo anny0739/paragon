06fa37939f5f4d10a56572b608ec3d184641b36c
anny0739 <anny@spoqa.com>
2015-08-01 23:58:54 +0900

    first commit

---
+++ app/src/main/java/com/paragonproject/ezwel/paragonproject/LoginActivity.java
@@ -0,0 +1,80 @@
+package com.paragonproject.ezwel.paragonproject;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+
+public class LoginActivity extends Activity {
+
+    public static final int REQUEST_CODE_LOGIN = 1001;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        Button button1 = (Button) findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getBaseContext(), MenuActivity.class);
+                startActivityForResult(intent, REQUEST_CODE_LOGIN);
+            }
+        });
+    }
+
+    protected void onActivityResult(int requestCd, int resultCd, Intent Data) {
+        super.onActivityResult(requestCd, resultCd, Data);
+
+        if( resultCd == 1 ) {
+            new AlertDialog.Builder(this).setTitle("팝업").setMessage("팝업1")
+                    .setNeutralButton("닫기", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                        }
+                    }).show();
+        } else if ( resultCd == 2 ) {
+            new AlertDialog.Builder(this).setTitle("팝업").setMessage("팝업2")
+                    .setNeutralButton("닫기", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                        }
+                    }).show();
+        } else if ( resultCd == 3 ) {
+            new AlertDialog.Builder(this).setTitle("팝업").setMessage("팝업3")
+                    .setNeutralButton("닫기", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                        }
+                    }).show();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_login, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
