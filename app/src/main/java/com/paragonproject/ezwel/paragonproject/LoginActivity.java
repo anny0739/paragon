8582af9d9782a46ca8c99152386c6c4221a2db8e
tools <hotbase@nate.com>
2015-08-19 14:21:55 +0900

    네이버 지도 작업중

--- app/src/main/java/com/paragonproject/ezwel/paragonproject/LoginActivity.java
+++ app/src/main/java/com/paragonproject/ezwel/paragonproject/LoginActivity.java
@@ -4,13 +4,13 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+
 
 
 public class LoginActivity extends Activity {
@@ -26,7 +26,7 @@ public class LoginActivity extends Activity {
             public void onClick(View v) {
                 Intent intent = new Intent(getBaseContext(), MenuActivity.class);
                 startActivityForResult(intent, REQUEST_CODE_LOGIN);
-                Log.d("msg","junGitTest2");
+                Log.d("msg",getApplicationContext().getPackageName());
             }
         });
     }
